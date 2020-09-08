db29936f4a04a80bfd539985c75118130abda60b
Ričardas Noreika <36963057+RicardasN@users.noreply.github.com>
2020-09-08 15:32:04 +0300

    chore: add main index.js file where all the functions/components should get exposed to the outside

---
+++ index.js
@@ -0,0 +1,5 @@
+import createTVSideNavigator from './SideNavigator/createTVSideNavigator';
+import TVSideNavigator from './SideNavigator/TVSideNavigator';
+import Overlay from './SideNavigator/Overlay';
+
+export default { createTVSideNavigator, TVSideNavigator, Overlay };
